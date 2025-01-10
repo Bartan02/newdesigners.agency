a0e9d3b64ac595c412927a5b186d63c6d78f21a3
Bartosz Adamczyk <adam0016@hz.nl>
2025-01-10 17:22:31 +0100

    Upload HSiF page

--- pages/index.vue
+++ pages/index.vue
@@ -1,123 +1,149 @@
 <template>
   <main>
-    <!-- Hero -->
-    <section class="relative flex items-center justify-center h-screen pt-0 pb-0 text-center bg-primary">
-
-      <video playsinline loop muted autoplay class="absolute top-0 left-0 z-0 w-full h-full object-cover opacity-40">
-        <source src="~/assets/videos/showreelND.mp4" type="video/mp4">
-      </video>
-
-      <div class="relative container text-white">
-        <h1 class="mb-2.5 md:text-6xlxl">YOUR PERSONAL DESIGN AGENCY</h1>
-        <!-- <p class="md:text-3xl text-1xl mx-auto max-w-4xl">
-          We specialize in digital design</a>. We love creating websites with
-          beautiful UX, but we also rebrand</a> and make <a href="/work"
-            class="text-secondary">videos</a>.
-        </p> -->
-
-        <p class="md:text-3xl text-1xl mx-auto max-w-4xl">
-          Whether you need a new <a href="/work" class="text-secondary">logo and branding</a> guidelines, a responsive and user-friendly <a href="/work" class="text-secondary">website</a>, or a stunning <a href="/work" class="text-secondary">brand video</a> that tells your company's story, we've got you covered!
-        </p>
-      </div>
-
-    </section>
-
-    <!-- Who we are -->
-    <section id="who-we-are" class="bg-gray">
-      <div class="container">
-
-        <h2>Hello, how can we help you?</h2>
-        <p class="mb-2 mx-auto text-center">We specialize in helping businesses become more attractive and balanced in their media presence, both online and offline. From creating new branding to producing engaging videos, our team of experts will work with you every step of the way to ensure that your company stands out from the competition.</p>
-        <div>
-          <img src="~/assets/images/new-designers-office.png" class="block mx-auto">
-        </div>
-        <div>
+    <!-- HSiF hero section -->
+    <section class="pt-0 pb-0 relative">
+      <div class="relative">
+        <Herobackground class="w-full"/>
+        <div class="absolute left-1/2 bottom-1/2 transform -translate-x-1/2 translate-y-3/7 md:w-hsiflogo md:h-hsiflogo w-hsiflogomobile h-hsiflogomobile bg-realwhite rounded-full">
+          <Hsif class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-auto w-3/4"/>
         </div>
       </div>
     </section>
 
-    <!-- What we do -->
-    <section id="what-we-do">
-      <div class="container">
-        <h2>CREATED FOR YOU</h2>
-        <Services />
-      </div>
-    </section>
-
-    <Testimonial />
-
-    <!--Preview-->
-    <section>
-      <vue-marquee-slider class="pb-3"
-        id="marquee-slider-loop"
-        :speed="3000"
-        :width="450"
-        :space="0">
-           <h2 class="text-6xl"> <span class="px-2">&bull;</span> OUR WORK</h2>
-        </vue-marquee-slider>
-
-        <div class="container grid lg:grid-cols-2 gap-1 md:gap-2 text-center">
-          <div class="column items-stretch">
-            <img class="h-24" src="../assets/images/work/taalgebruik-book.jpg">
-            <!-- <img src="../assets/images/projects/hsif 1.png">
-            <img src="../assets/images/projects/joint-cyber-range 1.png"> -->
-          </div>
-          <div class="column items-stretch">
-            <img src="../assets/images/projects/NextFashionRetail_scaled2.jpg">
-            <!-- <img class="h-24" src="../assets/images/projects/eu-impacthub 1.png">
-            <img class="h-24" src="../assets/images/projects/applied-research-project 1.png"> -->
-          </div>
-          <div class="column items-stretch">
-            <img class="h-24" src="../assets/images/projects/Joint-Cyberrange.jpg">
-          </div>
-          <div class="column items-stretch">
-            <img src="../assets/images/projects/hsif-1.png">
-          </div>
-          <div class="column items-stretch">
-            <img class="h-24" src="../assets/images/projects/Gemeente-Haag-Archief.jpg">
-          </div>
-          <div class="column items-stretch">
-            <img src="../assets/images/projects/European-Impact-Hub.jpg">
+    <!-- Description section -->
+    <section class="px-2 md:px-9">
+      <div class="md:flex md:flex-row md:gap-4">
+        <div class="relative text-realwhite w-min h-min md:self-start mx-auto">
+          <ContentVector class="w-18 h-18 md:w-auto md:h-auto"/>
+          <div class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <ul class="list-inside text-xl md:text-4xl font-bold w-max" :style="{ listStyleImage: `url(${listStyleImage})`}">
+              <li>
+                UX/UI
+              </li>
+              <li>
+                Web Design
+              </li>
+              <li>
+                Research
+              </li>
+              <li>
+                8 weeks
+              </li>
+            </ul>
           </div>
         </div>
-        <div class="flex justify-center pt-2">
-        <nuxt-link to="/work" class="nav-link btn btn-sm self-center">
-          Take a look at our projects
-        </nuxt-link>
-      </div>
-
-    </section>
-
-    <!-- Client -->
-    <section id="clients" class="bg-gray">
-      <div class="container text-center">
-        <div>
-          <img src="~/assets/images/how-we-work.png" class="block mx-auto">
+        <div class="flex flex-col justify-around md:mt-0 mt-2 md:gap-0 gap-2">
+          <div>
+            <h5 class="text-2xl">
+              Background
+            </h5>
+            <span>
+              It is a startup company working to help students and young people in the field. They presented us with a project aimed at creating their own website to introduce their customers to them.
+            </span>
+          </div>
+          <div>
+            <h5 class="text-2xl">
+              Goal
+            </h5>
+            <span>
+              We did an in-depth study of all users and then we created prototypes and tested them. Based on the results, we created the final design and tested it again. It took us around 8 weeks to finish this project, and we are glad to share the steps with you!
+            </span>
+          </div>
         </div>
-        <h2 class="mt-2">GET STARTED</h2>
-        <p class="mb-2 mx-auto">
-          At New Designers, we create custom solutions tailored to your needs. After a personal consultation, we'll provide a fixed-price package that meets your goals and budget.
-        </p>
-        <nuxt-link to="/get-started" class="btn">Contact us to get started today!</nuxt-link>
       </div>
+      <!-- Gallery -->
+      <div class="flex justify-center pt-4">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="383" height="383" viewBox="0 0 383 383" fill="none" class="h-1/2 md:h-auto md:w-full">
+          <defs>
+            <pattern id="pattern_image_1" patternUnits="userSpaceOnUse" width="383" height="383">
+              <image xlink:href="~/assets/images/hsif/image1.jpg" x="0" y="0" width="383" height="383" preserveAspectRatio="xMinYMin"/>
+            </pattern>
+          </defs>
+          <path d="M383 25L0 0L25 382.5L357.5 358.5L383 25Z" fill="url(#pattern_image_1)"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="383" height="383" viewBox="0 0 383 383" fill="none" class="h-1/2 md:h-auto md:w-full">
+          <defs>
+            <pattern id="pattern_image_2" patternUnits="userSpaceOnUse" width="383" height="383">
+              <image xlink:href="~/assets/images/hsif/image2.jpg" x="0" y="0" width="383" height="383" preserveAspectRatio="xMinYMin"/>
+            </pattern>
+          </defs>
+          <path d="M0 357.5L383 382.5L358 0L25.5 24L0 357.5Z" fill="url(#pattern_image_2)"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="383" height="383" viewBox="0 0 383 383" fill="none" class="h-1/2 md:h-auto md:w-full">
+          <defs>
+            <pattern id="pattern_image_3" patternUnits="userSpaceOnUse" width="383" height="383">
+              <image xlink:href="~/assets/images/hsif/image3.jpg" x="0" y="0" width="383" height="383" preserveAspectRatio="xMinYMin"/>
+            </pattern>
+          </defs>
+          <path d="M383 25L0 0L25 382.5L357.5 358.5L383 25Z" fill="url(#pattern_image_3)"/>
+        </svg>
+      </div>
+      <!-- End of gallery -->
+      <span class="flex font-bold text-2xl justify-center pt-3">
+        Visit website &#8594;
+      </span>
     </section>
 
+    <!-- Request a project section -->
+    <section class="px-2 md:px-9 relative">
+      <div>
+        <h5 class="text-5xl pb-1">Speed up your digital growth together?</h5>
+        <span class="text-2xl">
+        You can leave your project information, we will review it, and we will contact you! <br/>
+        After that we will have a free discussion of your project.
+      </span>
+      </div>
+      <div class="pt-2 md:pt-0 flex justify-end">
+        <button class="btn">
+          Request project
+        </button>
+      </div>
+      <!-- Decorations -->
+      <div class="absolute bottom-0 left-0">
+        <svg xmlns="http://www.w3.org/2000/svg" width="116" height="177" viewBox="0 0 116 177" fill="none">
+          <path d="M15.7306 372.322L113.952 113.94L-195.625 2.00124L-249.09 325.175L15.7306 372.322Z" stroke="#FFBA00" stroke-width="2"/>
+        </svg>
+      </div>
+      <div class="absolute -top-4 md:top-4 right-2">
+        <svg width="89" height="104" viewBox="0 0 89 104" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M81.4579 62.9448L39.9657 100.407L0.000660453 56.6362L56.2302 21.6024L81.4579 62.9448Z" fill="#4E5B73"/>
+          <path d="M20.8986 58.636L69.6044 60.0234L70.3104 2.00001L13.4099 11.8165L20.8986 58.636Z" stroke="#FFBA00" stroke-width="2"/>
+        </svg>
+      </div>
+      <!-- End of decorations -->
+    </section>
   </main>
 </template>
 
 <script>
 import Vue from 'vue'
 import VueMarqueeSlider from 'vue-marquee-slider';
+import Herobackground from '~/assets/images/hsif/herobackground.svg';
+import Hsif from '~/assets/images/hsif/HSiF.svg';
+import ContentVector from '~/assets/images/hsif/contentvector.svg';
 Vue.use(VueMarqueeSlider)
 
 
 export default {
+  components: {
+    Herobackground,
+    Hsif,
+    ContentVector
+  },
   head() {
     return {
       script: [
         { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }
       ]
     };
+  },
+  data() {
+    return {
+      listStyleImage: require('~/assets/css/components/ulfinger.png'),
+    };
+  },
+  mounted() {
+    console.log(this.listStyleImage);
   }
 };
 </script>
